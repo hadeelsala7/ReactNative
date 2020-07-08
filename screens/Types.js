fea7cf2e1f1be58e9d22741ee142026f65e73c21
hadeel salah <hadeelsalah80@gmail.com>
2020-07-09 01:44:22 +0300

    Build types

--- screens/Types.js
+++ screens/Types.js
@@ -1,55 +1,68 @@
-import React  from 'react';
+import React ,{Component} from 'react';
+import axios from 'react-native-axios';
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
 } from 'react-native';
-function Types() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../images/home.jpg')} style={styles.container}>
-        <View style={styles.elements}>
-           <Text style={styles.firsth}>Welcome </Text>
-        </View>
-     </ImageBackground>
-    </View> 
-  );
-}
+class Types extends Component {
+
+  state = {
+    types: [],
+  }
+  componentDidMount() {
+    axios.get(`http://localhost:5000/users`)
+      .then(res => {
+        this.setState( {types: res.data}
+        );
+        
+      })
+  }
+  render(){
+    console.log(this.state.types);
+    return (
+      <View style={styles.container}>
+          <View style={styles.elements}>
+            {this.state.types.map(user =>{
+                return(
+                  <Text style={styles.textstyle} key={user.id}>{user.name}</Text>
+              )
+        })} 
+          </View>
+      </View> 
+    );
+  }
+  }
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     width: null,
     height: null,
+    backgroundColor: '#9932CC'
+
     
   },
   elements:{
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  button:{
-    width: '70%', 
-    height: 50, 
-    backgroundColor: '#800080', 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: -400,
-    borderRadius: 15
+   display: "flex",
+   flexWrap: "wrap",
+   margin:10,
+   justifyContent:"center",
+   flexDirection: 'row',
 
   },
   textstyle:{
-    color: '#fff',
-    fontSize:22,
-
-  },
-  firsth:{
-    fontSize: 50,
-    position: 'absolute',
-    top: 200,
-    fontWeight: 'bold'
-
+    fontSize:12,
+    backgroundColor: "white",
+    color: 'black',
+    fontWeight: 900,
+    textAlign: "center",
+    width:150,
+    height: 40,
+    margin:10,
+    borderRadius: 15,
+    padding:10,
 
   }
 });
